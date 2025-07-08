6aac78fbb9ea91a29c5cefca29e4530d744347f7
Ryan Lee <ryan.lee@sarcomeredynamics.com>
2025-07-08 10:28:58 -0700

    Fix GUIv2 gui.bat windows script

--- examples/Control/ArtusLiteControl/GUIControlV2/gui.bat
+++ examples/Control/ArtusLiteControl/GUIControlV2/gui.bat
@@ -1,13 +1,10 @@
+
 @echo off
-REM Set variable for python path (update the path accordingly)
-@REM set PYTHON_PATH="C:\path\to\your\venv\Scripts\python.exe"
-PYTHON_PATH="python"
-
-REM Launch first terminal and run the first script
+@REM REM Set variable for python path (update the path accordingly)
+set PYTHON_PATH=%~dp0venv\Scripts\python.exe
+@REM REM Launch first terminal and run the first script
 start "Artus Lite GUI" cmd /k "%PYTHON_PATH% ..\..\Tracking\gui_data_v2\artus_lite_gui.py & echo Press Enter to close... & pause"
-
-REM Wait for 5 seconds before launching the next terminal
+@REM REM Wait for 5 seconds before launching the next terminal
 timeout /t 5
-
-REM Launch second terminal and run the second script
+@REM REM Launch second terminal and run the second script
 start "GUI Controller" cmd /k "%PYTHON_PATH% gui_controller.py & echo Press Enter to close... & pause"
